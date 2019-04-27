25b1dcd12efaa243efaa7ca226e5a76f1c734ee3
鸽子 <chengyu2333@gmail.com>
2019-04-27 13:20:04 +0800

    日志优化

--- log.py
+++ log.py
@@ -4,32 +4,31 @@ import os
 import sys
 
 
-if not os.path.exists("logs"):
-    os.makedirs("logs")
+log_path = "logs"
+
+if not os.path.exists(log_path):
+    os.makedirs(log_path)
 
 
 def log_error(msg):
-    f = io.open("logs/" + time.strftime("%Y-%m-%d", time.localtime()) + " errors.log", 'a', encoding="utf-8")
+    f = io.open(log_path + "/error-" + time.strftime("%Y-%m-%d", time.localtime()) + ".log", 'a', encoding="utf-8")
     log = time.ctime() + " | \t" + msg + "\r\n\n"
-    # print("\033[1;31m" + str(log) + "\033[0m")
     sys.stdout.write("\033[1;31m" + str(log) + "\033[0m")
     f.write(log)
     f.close()
 
 
 def log_info(msg):
-    f = io.open("logs/" + time.strftime("%Y-%m-%d", time.localtime()) + " info.log", 'a', encoding="utf-8")
+    f = io.open(log_path + "/info-" + time.strftime("%Y-%m-%d", time.localtime()) + ".log", 'a', encoding="utf-8")
     log = time.ctime() + " | \t" + msg + "\r\n\n"
-    # print("\033[1;33m" + str(log) + "\033[0m")
     sys.stdout.write("\033[1;32m" + str(log) + "\033[0m")
     f.write(log)
     f.close()
 
 
 def log_success(msg):
-    f = io.open("logs/" + time.strftime("%Y-%m-%d", time.localtime()) + " success.log", 'a', encoding="utf-8")
+    f = io.open(log_path + "/success-" + time.strftime("%Y-%m-%d", time.localtime()) + ".log", 'a', encoding="utf-8")
     log = time.ctime() + " | \t" + msg + "\r\n\n"
-    # print("\033[1;33m" + str(log) + "\033[0m")
     sys.stdout.write("\033[1;33m" + str(log) + "\033[0m")
     f.write(log)
     f.close()
